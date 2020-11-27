47e82068b42a2d2d1cec72485e54f555f98b6043
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-11-27 15:45:37 +0800

    Prepare for dokku deployment

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,19 @@
 import Config
 
-import_config "secrets.exs"
+token = System.get_env("TOKEN")
+
+case token do
+  nil ->
+    import_config "secrets.exs"
+
+  x when is_binary(x) ->
+    config :ex_gram, token: token
+
+    base_url =
+      System.get_env("BASE_URL") ||
+        raise """
+        environment variable BASE_URL is missing.
+        """
+
+    config :rent_division_telegram, base_url: base_url
+end
